ca41f96e0668baba62e2342c6f561e9708932eeb
Baplisca <yokota0327@gmail.com>
2023-02-12 14:21:58 +0900

    add volta to fix node version and create navbar UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import NextNProgress from "nextjs-progressbar";
+import "../../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
