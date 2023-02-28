420ed2a5819e68e82b421cfb88dc3dd38ea78f34
Baplisca <yokota0327@gmail.com>
2023-02-28 22:14:17 +0900

    performance tuning img lazy loading

--- src/organisms/FreeTimeContent.tsx
+++ src/organisms/FreeTimeContent.tsx
@@ -36,6 +36,7 @@ const freeTimeItems: IAboutContent[] = [
         <img
           src="https://img.shields.io/endpoint?url=https%3A%2F%2Fatcoder-badges.now.sh%2Fapi%2Fatcoder%2Fjson%2FBaplisca"
           alt="競技プログラミング レート"
+          loading="lazy"
         />
       </a>
     ),
