69ff6ebefb3e81620f3f1e937144ebbdfa9d3ef5
Baplisca <yokota0327@gmail.com>
2023-04-08 16:37:07 +0900

    bug fix

--- src/pages/about_en.tsx
+++ src/pages/about_en.tsx
@@ -1,9 +1,15 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import Navbar from "../organisms/Navbar";
+import { useLanguageStore } from "../stores/language-store";
+import { usePageStore } from "../stores/page-store";
 import AboutTemplate from "../templates/AboutTemplate";
 
 const AboutEn: NextPage = () => {
+  const languageStore = useLanguageStore();
+  if (!languageStore.isEnglish) languageStore.setLanguage("En");
+  const pageStore = usePageStore();
+  if (pageStore.page != "about") pageStore.setPage("about");
   return (
     <>
       <Head>
--- src/pages/work_en.tsx
+++ src/pages/work_en.tsx
@@ -1,6 +1,12 @@
 import Navbar from "../organisms/Navbar";
+import { useLanguageStore } from "../stores/language-store";
+import { usePageStore } from "../stores/page-store";
 
 const Work = () => {
+  const languageStore = useLanguageStore();
+  if (!languageStore.isEnglish) languageStore.setLanguage("En");
+  const pageStore = usePageStore();
+  if (pageStore.page != "work") pageStore.setPage("work");
   return (
     <>
       <Navbar />
